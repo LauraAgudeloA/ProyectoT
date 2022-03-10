bb35cffb03eeded3c81ebb23ce1475f4e1097614
Laura <lauraagudelo102001@gmail.com>
2022-03-10 10:01:27 -0500

    Actualización de proyecto para truora

---
+++ js/cod.js
@@ -0,0 +1,76 @@
+function myFunction(x) {
+    if (x.matches) {
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(2)').style.display='none;';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(3)').style.display='none';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginTop = '0px';
+        document.querySelector("body > main > div:nth-child(7)").style.padding = '1px';
+        document.querySelector("body > main > div:nth-child(2) > div > div.left > div:nth-child(3) > a").style.marginLeft = '28%';
+        document.querySelector("body > main > div:nth-child(2) > div > div.left").style.width = '500px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div.big-title").style.width = '500px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginLeft = '28%';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div.rowtext > div:nth-child(1) > h4").style.marginLeft = '30%';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)").style.marginTop = '30px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)").style.textAlign = 'center';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)").style.marginTop = '30px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)").style.textAlign = 'center';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(1) > div > div > p:nth-child(3)").style.marginLeft = '15px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(1) > div > div > p:nth-child(4)").style.marginLeft = '20px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(1)").style.paddingLeft = '20px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(2)").style = '';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(3)").style.paddingTop = '100px';
+        document.querySelector("body > main > div:nth-child(4) > div > div").style.marginLeft = '0';
+        document.querySelector("body > main > div.showcase-area5 > div").style.padding = '30px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(3) > p").style.marginTop = '-100px';
+    } else {
+        document.querySelector("body > main > div:nth-child(7)").style.padding = '18px';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(2)').style.display='';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(3)').style.display='';
+        document.querySelector("body > main > div:nth-child(4) > div > div").style.marginLeft = '10%';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div.big-title").style.width = '';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginTop = '40px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginLeft = '-20px';
+    }
+}
+
+function myFunction2(y) {
+    if (y.matches) {
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(2)').style.display='none';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(3)').style.display='none';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginTop = '0px';
+        document.querySelector("body > main > div:nth-child(7)").style.padding = '1px';
+        document.querySelector("body > main > div:nth-child(2) > div > div.left > div:nth-child(3) > a").style.marginLeft = '28%';
+        document.querySelector("body > main > div:nth-child(2) > div > div.left").style.width = '500px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div.big-title").style.width = '500px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginLeft = '28%';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div.rowtext > div:nth-child(1) > h4").style.marginLeft = '30%';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)").style.marginTop = '30px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)").style.textAlign = 'center';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)").style.marginTop = '30px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(2) > div:nth-child(3)").style.textAlign = 'center';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(1) > div > div > p:nth-child(3)").style.marginLeft = '15px';
+        document.querySelector("body > main > footer > div:nth-child(1) > div:nth-child(1) > div > div > p:nth-child(4)").style.marginLeft = '20px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(1)").style.paddingLeft = '20px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(2)").style = '';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(3)").style.paddingTop = '100px';
+        document.querySelector("body > main > div:nth-child(4) > div > div").style.marginLeft = '0';
+        document.querySelector("body > main > div.showcase-area5 > div").style.padding = '30px';
+        document.querySelector("body > main > footer > div:nth-child(2) > div:nth-child(3) > p").style.marginTop = '-100px';
+    } else {
+        document.querySelector("body > main > div:nth-child(7)").style.padding = '18px';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(2)').style.display='';
+        document.querySelector('body > main > div.showcase-area5 > div > div > div:nth-child(3)').style.display='';
+        document.querySelector("body > main > div:nth-child(4) > div > div").style.marginLeft = '10%';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div.big-title").style.width = '';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginTop = '40px';
+        document.querySelector("body > main > div:nth-child(7) > div > div.left1 > div:nth-child(3) > a").style.marginLeft = '-20px';
+    }
+}
+
+var y = window.matchMedia("(max-width: 470px)")
+myFunction2(y)
+y.addListener(myFunction2)
+
+var x = window.matchMedia("(max-width: 870px)")
+myFunction(x)
+x.addListener(myFunction)
+
